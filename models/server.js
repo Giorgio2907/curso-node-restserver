75d9f3d9d26577c2e18577cc1251d4d4d797ce22
Giorgio2907 <imanol.ticona@unmsm.edu.pe>
2026-06-30 00:35:12 -0500

    fix: asegurando puerto dinamico process.env.PORT

--- models/server.js
+++ models/server.js
@@ -5,7 +5,7 @@ class Server {
 
     constructor() {
         this.app  = express();
-        this.port = process.env.PORT || 8080;
+        this.port = process.env.PORT || 3000;
         
         // Definimos la propiedad con la ruta base de nuestros usuarios
         this.usuariosPath = '/api/usuarios';
